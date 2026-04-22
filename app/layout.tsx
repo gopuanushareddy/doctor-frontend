8bc90dbb6b7c41b6fba3e2c7df5a1d9297598011
gopuanushareddy <gopuanushareddy@gmail.com>
2026-04-22 17:38:39 +0530

    second

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,15 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Cursor from "../components/Cursor";
+import { ReactNode } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
-    <html>
+    <html lang="en">
       <body className="bg-gradient-to-br from-white via-pink-50 to-purple-50">
         <Cursor />
         <Navbar />
